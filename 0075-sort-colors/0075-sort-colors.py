26c4b79f8d46e4273237f872cb28a0d4495b6e64
Kemal <109658498+hawt24@users.noreply.github.com>
2023-01-20 17:13:56 +0300

    Time: 40 ms (58.02%), Space: 13.9 MB (58.40%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,4 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        left,r=0,len(nums)-1
+        i=0
+        
+        def swap(i,j):
+          temp=nums[i]
+          nums[i]=nums[j]
+          nums[j]=temp
+        while i<=r:
+            if nums[i]==0:
+              swap(left,i)
+              left+=1
+            elif nums[i]==2:
+                swap(i,r)
+                r-=1
+                i-=1
+            i+=1
+            

4f8dd04587fc7cdc7e8f090dd2016a5228655bc8
Kemal <109658498+hawt24@users.noreply.github.com>
2023-02-07 18:59:42 +0300

    Time: 920 ms (81.84%), Space: 20.2 MB (51.27%) - LeetHub

---
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        start=0
+        end=0
+        dic={}
+        max_len=0
+        while end<len(fruits):
+            dic[fruits[end]]=end
+            if len(dic)>=3:
+                min_val=min(dic.values())
+                del dic[fruits[min_val]]
+                start=min_val+1
+            max_len=max(max_len,end-start+1)
+            end+=1
+        return max_len

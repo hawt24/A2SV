936d7945916d8bed436dfacb95d7909f1f597b00
Kemal <109658498+hawt24@users.noreply.github.com>
2023-02-28 16:17:06 +0300

    Time: 218 ms (24.08%), Space: 43.3 MB (5.49%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,11 +3,13 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        prev_index=0
-        i=len(s)-1
-        while prev_index<i:
-            s[prev_index],s[i]=s[i],s[prev_index]
-            prev_index+=1
-            i-=1
-            
         
+        def ponters(left,right):
+            if left>=right:
+                return 
+            s[left],s[right]=s[right],s[left]
+            ponters(left+1,right-1)
+        ponters(0,len(s)-1)
+
+             
+       

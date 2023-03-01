b2760e99cd322df02182c6f613e0f01fe4d0c4d0
Kemal <109658498+hawt24@users.noreply.github.com>
2023-03-01 12:12:55 +0300

    Time: 4717 ms (12.66%), Space: 13.8 MB (98.78%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,13 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        def helper(start,end):
+            if start==end:
+                return nums[start]
+            left=nums[start]
+            right=nums[end]
+            left_op=helper(start+1,end)
+            right_op=helper(start,end-1)
+            return max(left-left_op,right-right_op)
+        temp=helper(0,len(nums)-1)
+            
+        return temp >= 0

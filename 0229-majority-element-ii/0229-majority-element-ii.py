9187b3f2f363d3911217d0f9c3e33919db17ca51
Kemal <109658498+hawt24@users.noreply.github.com>
2023-03-27 22:20:42 +0300

    Time: 156 ms (5.15%), Space: 15.5 MB (15.38%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        if len(nums)==len(set(nums)) and len(nums)<=2:
+            return nums
+        
+        cnt=Counter(nums)
+        ans=[]
+        for i in range(len(nums)):
+            print(cnt[nums[i]])
+            if cnt[nums[i]]>len(nums)//3:
+                ans.append(nums[i])
+        return list(set(ans))
+        

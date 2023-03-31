536d9086d678c3699f491b03ecfcbf01e7954f40
Kemal <109658498+hawt24@users.noreply.github.com>
2023-03-31 16:38:18 +0300

    Time: 128 ms (89.92%), Space: 15.1 MB (65.95%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,14 +1,38 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n=len(nums)
-        missed=0
-        sum_=int((n*(n+1))/2) #arthematic progression
-        for i in nums:
-            missed+=i
-        return sum_-missed
+        #     n=len(nums)
+        #     missed=0
+        #     summ=int((n*(n+1))/2) #arthematic progression
+        #     for i in nums:
+        #         missed+=i
+        #     return summ-missed
         
-    
+        # i=0
+        # n=len(list)
+        # while(i<n):
+        #     if(i==list[i]):
+        #         i+=1
+        #     elif(list[i]>=n):
+        #         flag=i
+        #         i+=1
+        #     else:
+        #         correct= list[i]
+        #         list[i],list[correct] =list[correct],list[i]
+        # return flag
+        
+        # counter = [iterator for iterator in range(len(nums)+1)]
+        # print(counter)
+        
+        total=0
+        actual_num=0
+        for i in range(len(nums)):
+            total^=i
+            actual_num^=nums[i]
+        total^=len(nums)
+        return total^actual_num
             
+
+   
         
         
         

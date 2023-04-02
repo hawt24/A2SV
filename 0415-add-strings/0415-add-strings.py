5fcdb84c6a38610e410d222097de6232b23b50af
Kemal <109658498+hawt24@users.noreply.github.com>
2023-04-02 23:10:13 +0300

    Time: 41 ms (49.27%), Space: 13.9 MB (44.16%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.py
@@ -0,0 +1,5 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        digits_integer1 = int(''.join(map(str,num1)))
+        digits_integer2 = int(''.join(map(str,num2)))
+        return str(digits_integer1+digits_integer2)

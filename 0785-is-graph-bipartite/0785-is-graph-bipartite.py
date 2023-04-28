4e7d144bbb51463750171e9c55ac8117af2add9d
Kemal <109658498+hawt24@users.noreply.github.com>
2023-04-28 19:28:47 +0300

    Time: 185 ms (26.75%), Space: 17.1 MB (5.02%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -0,0 +1,39 @@
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        
+        arr=[-1]*len(graph)
+        
+        def dfs(i):
+            for child in graph[i]:
+                if arr[child]==-1:
+                    if arr[i]==0:
+                        arr[child]=1
+                    else:
+                        arr[child]=0
+                        
+                    if not dfs(child):
+                        return False
+                elif arr[i]==arr[child]:
+                    return False
+            return True
+        
+        for i in range(len(graph)):
+            if arr[i]==-1:
+                arr[i]=0
+                if not dfs(i):
+                    return False
+        return True
+                 
+            
+                
+            
+        
+        
+
+        
+        
+     
+            
+        
+        
+        

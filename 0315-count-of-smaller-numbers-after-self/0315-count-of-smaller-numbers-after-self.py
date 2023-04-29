8ad1835120850a45bf9756865511394ff0e7b131
Kemal <109658498+hawt24@users.noreply.github.com>
2023-04-29 17:41:49 +0300

    Time: 2053 ms (98.82%), Space: 36.8 MB (50.20%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -9,7 +9,7 @@ class Solution:
         #             cnt+=1
         #     arr[i]=cnt
         # return arr
-        
+        """when using append list is use time compexity of o(n) but we can use soretdlist from m           sorted continer so the add in this list is the complexity is O(logn)  beacause the index is         found"""
         temp=SortedList([])
         ans=[]
         for i in range(len(nums)-1,-1,-1):
@@ -17,6 +17,7 @@ class Solution:
             idx=temp.bisect_left(num)
             ans.append(idx)
             temp.add(num)
+        
         return ans[::-1]
             
         

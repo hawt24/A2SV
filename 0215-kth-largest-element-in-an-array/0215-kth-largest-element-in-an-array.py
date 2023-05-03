57161acaff8c3f071d99dc0d2d00280f4d476849
Kemal <109658498+hawt24@users.noreply.github.com>
2023-05-03 18:34:50 +0300

    Time: 579 ms (33.75%), Space: 29 MB (29.14%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -3,15 +3,16 @@ class Solution:
         # nums.sort()
         # return nums[-k]
               
-        for i in range(len(nums)):
-            nums[i] *= -1
+        # for i in range(len(nums)):
+        #     nums[i] *= -1
         
         heapify(nums)
         print(nums)
         
         ans = 0
-        while k>0:
-            ans = -heappop(nums)
-            k -= 1
+        temp = len(nums) - k + 1
+        while temp > 0:
+            ans = heappop(nums)
+            temp -= 1
         return ans
       

417aef31fd3e6b3d3991dd292941d773a2930aa5
Kemal <109658498+hawt24@users.noreply.github.com>
2023-05-10 21:12:02 +0300

    Time: 661 ms (70.95%), Space: 24.4 MB (13.35%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.py
@@ -0,0 +1,19 @@
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        
+        white, gray, black = 0, 1, 2
+        def dfs(node):
+            if colors[node] != white:
+                return colors[node] == black
+            colors[node] = gray
+            for neighbor in graph[node]:
+                if colors[neighbor] == black:
+                    continue
+                if colors[neighbor] == gray or not dfs(neighbor):
+                    return False
+            colors[node] = black
+            return True
+        n = len(graph)
+        colors = [white] * n
+        ans=[i for i in range(n) if dfs(i)]
+        return sorted(ans)

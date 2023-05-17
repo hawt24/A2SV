5a7a01baafa11b48b7fb8f423e5e77299c24fdbc
Kemal <109658498+hawt24@users.noreply.github.com>
2023-05-17 18:17:48 +0300

    Time: 1895 ms (72.29%), Space: 108.8 MB (52.99%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,26 +1,33 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        if n==1:
-            return True
-        graph=defaultdict(list)
-        for sorce,dest in edges:
-            graph[sorce].append(dest)
-            graph[dest].append(sorce)
+        graph=defaultdict(int)
+        
+        def find(x):
+            if x not in graph:
+                graph[x]=x
+                
+            while graph[x]!=x:
+                graph[x]=graph[graph[x]]
+                x=graph[x]
+                
+            return x
+        
+        def union(x,y):
+            p1,p2=find(x),find(y)
+            if p1>p2:
+                graph[p1]=p2
+            else:
+                graph[p2]=p1
+                
+        for i,j in edges:
+            union(i,j)
+                
+        return find(source)==find(destination)
+    
+
             
-        flag=False
-        def dfs(arr):
-            nonlocal flag,graph
-            for i in arr:
-                if i==destination:
-                    flag=True
-                    return
-                if i not in  visted:
-                    visted.add(i)
-                    dfs(graph[i])
-        visted=set()
-        dfs(graph[source])
-        return flag
-  
+        
+
             
         
             

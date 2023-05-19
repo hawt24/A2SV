e1fd81528f74094c832dffbc9f4899d9c43a8006
Kemal <109658498+hawt24@users.noreply.github.com>
2023-05-19 10:37:13 +0300

    Time: 63 ms (19.76%), Space: 16.7 MB (12.77%) - LeetHub

---
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -0,0 +1,28 @@
+class Solution:
+    def equationsPossible(self, equations: List[str]) -> bool:
+        set1, set2, set3 = set(), set(), set()
+        a, b = None, None
+        for item in equations:
+            if item[1] == "=":
+                a, b = item.split("==")
+                set2.add((a, b))
+            else:
+                a, b = item.split("!=")
+                set3.add((a, b))
+            set1 |= {a, b}
+        graph = {char: char for char in set1}
+        def find(char):
+            if graph[char] == char:
+                return char
+            return find(graph[char])
+        def union(x, y):
+            xrep, yrep = find(x), find(y)
+            if xrep != yrep:
+                graph[yrep] = xrep
+        for x, y in set2:
+            union(x, y)
+        for x, y in set3:
+            r1, r2 = find(x), find(y)
+            if r1 == r2:
+                return False
+        return True

ac43bb0a2860b7e168c193d46f32ef73cbec39e6
Kemal <109658498+hawt24@users.noreply.github.com>
2023-05-31 19:33:22 +0300

    Time: 53 ms (12.65%), Space: 16.6 MB (6.33%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -5,14 +5,14 @@ class Solution:
         
         memo = {}
         def dp(i, j):
-            if i == 0 and j == 0:
+            if i == m - 1 and j == n - 1:
                 return 1
-            if i < 0 or j < 0:
+            if i >= m or j >= n:
                 return 0
             if (i, j) in memo:
                 return memo[(i, j)]
-            ans = dp(i-1, j) + dp(i, j-1)
+            ans = dp(i+1, j) + dp(i, j+1)
             memo[(i, j)] = ans
             return ans
         
-        return dp(m-1, n-1)
+        return dp(0, 0)

dd9dc56abb3c9f82440f98b698dc5a002ed6f0d9
Kemal <109658498+hawt24@users.noreply.github.com>
2023-07-28 23:49:37 +0300

    Time: 3012 ms (25.09%), Space: 16.4 MB (70.06%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -9,5 +9,4 @@ class Solution:
             right_op=helper(start,end-1)
             return max(left-left_op,right-right_op)
         temp=helper(0,len(nums)-1)
-            
         return temp >= 0

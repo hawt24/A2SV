9f9867182538a0d892794030136d3b69481a437c
Kemal Siraj <109658498+hawt24@users.noreply.github.com>
2023-10-06 10:16:58 +0300

    Time: 1497 ms (17.74%), Space: 19.7 MB (98.42%) - LeetHub

---
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -0,0 +1,20 @@
+class Solution:
+    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        
+        dist=[[False for _ in range(numCourses)] for _ in range(numCourses)]
+        
+        for i,j in prerequisites:
+            dist[i][j]=True
+        
+        for k in range(numCourses):
+            for i in range(numCourses):
+                for j in range(numCourses):
+                    dist[i][j]=dist[i][j] or (dist[i][k] and dist[k][j])
+                    
+                    
+        ans=[]
+        for i,j in queries:
+            ans.append(dist[i][j])
+        return ans
+        
+        

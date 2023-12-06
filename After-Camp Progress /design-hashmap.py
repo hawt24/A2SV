6b2b99053757a157959c7771297b8b0bb7b5ff59
Kemal Siraj <109658498+hawt24@users.noreply.github.com>
2023-12-06 16:12:57 +0300

    Added solution for Design HashMap

---
+++ After-Camp Progress /design-hashmap.py
@@ -0,0 +1,22 @@
+class MyHashMap:
+
+    def __init__(self):
+        self.head=[-1]*(10**6+1)
+
+    def put(self, key: int, value: int) -> None:
+            self.head[key]=value
+            
+    def get(self, key: int) -> int:
+            return self.head[key]
+    def remove(self, key: int) -> None:
+        self.head[key]=-1
+        
+        
+        
+
+
+# Your MyHashMap object will be instantiated and called as such:
+# obj = MyHashMap()
+# obj.put(key,value)
+# param_2 = obj.get(key)
+# obj.remove(key)
